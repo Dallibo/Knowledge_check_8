5ece707ec026d5ad35a0667c359ec3b0c5bbed16
Dallibo <denismalyarenko1@gmail.com>
2023-08-05 13:25:12 +0300

    hw

--- Knowledge_check_8.js
+++ Knowledge_check_8.js
@@ -27,3 +27,36 @@ const timeCar1 = calculateTravelTime(distanceToTravel, car1.speed)
 const timeCar2 = calculateTravelTime(distanceToTravel, car2.speed)
 console.log(timeCar1)
 console.log(timeCar2)
+
+const Fraction = {
+    create(numerator, denominator) {
+      return { numerator, denominator };
+    },
+  
+    add(fraction1, fraction2) {
+      const commonDenominator = fraction1.denominator * fraction2.denominator;
+      const newNumerator = fraction1.numerator * fraction2.denominator + fraction2.numerator * fraction1.denominator;
+      return this.create(newNumerator, commonDenominator);
+    },
+  
+    gcd(a, b) {
+      return b === 0 ? a : this.gcd(b, a % b);
+    },
+  
+    simplify(fraction) {
+      const gcd = this.gcd(fraction.numerator, fraction.denominator);
+      fraction.numerator /= gcd;
+      fraction.denominator /= gcd;
+    },
+  
+    toString(fraction) {
+      return `${fraction.numerator}/${fraction.denominator}`;
+    },
+  };2
+  
+  const fraction1 = Fraction.create(1, 6);
+  const fraction2 = Fraction.create(1, 5);
+  
+  const result = Fraction.add(fraction1, fraction2);
+  Fraction.simplify(result);
+  console.log(Fraction.toString(result));
